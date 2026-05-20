67084ad95f5bcf3a120f549977a23ce5eec5134f
Ilya Belenkiy <ilya.belenkiy@whoop.com>
2026-05-20 11:49:20 -0400

    [AIX-1360] Add single-pass Markdown layout defaults

--- Sources/MarkdownUI/Theme/BlockStyle/ListBullet.swift
+++ Sources/MarkdownUI/Theme/BlockStyle/ListBullet.swift
@@ -2,21 +2,12 @@ import SwiftUI
 
 struct ListBullet: View {
   private let image: Image
-  private let verticalOffset: CGFloat
+  let verticalOffset: CGFloat
 
   var body: some View {
     TextStyleAttributesReader { attributes in
       let fontSize = attributes.fontProperties?.scaledSize ?? FontProperties.defaultSize
-      let bullet = self.image.font(.system(size: round(fontSize / 3)))
-
-      if #available(iOS 16.0, macOS 13.0, tvOS 16.0, watchOS 9.0, *) {
-        bullet.layoutValue(
-          key: ListMarkerVerticalOffsetLayoutValueKey.self,
-          value: self.verticalOffset
-        )
-      } else {
-        bullet
-      }
+      self.image.font(.system(size: round(fontSize / 3)))
     }
   }
 
